6c24a4723c58014c0d3c815177c9d4c3ac77740e
Ryan Hellyer <ryanhellyer@gmail.com>
2016-01-20 16:46:10 +0100

    Explaining how the autoloader works

--- media-manager.php
+++ media-manager.php
@@ -30,6 +30,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 /**
  * Autoload the classes.
+ * Includes the classes, and automatically instantiates them via spl_autoload_register().
  *
  * @param  string  $class  The class being instantiated
  */
