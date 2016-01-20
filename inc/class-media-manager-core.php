7ab46e62eb6fa2a00f6961a0b6ff491b6ce53731
Ryan Hellyer <ryanhellyer@gmail.com>
2016-01-20 16:50:49 +0100

    Documenting protected methods

--- inc/class-media-manager-core.php
+++ inc/class-media-manager-core.php
@@ -15,6 +15,7 @@ abstract class Media_Manager_Core {
 	/**
 	 * Get the post-types intended for deletion.
 	 *
+	 * @access  protected
 	 * @return  array  The post-types which should have their attachments deleted.
 	 */
 	protected function get_post_types() {
@@ -31,6 +32,7 @@ abstract class Media_Manager_Core {
 	/**
 	 * Get the taxonomies intended for deletion.
 	 *
+	 * @access  protected
 	 * @return  array  The taxonomies which should have their attachments deleted.
 	 */
 	protected function get_post_taxonomies() {
